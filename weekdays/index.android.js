c9b54ac8c73cb8e91fbff4a0721c392abd4bab35
kevinw123 <kevinwong_895@hotmail.com>
2017-06-15 16:43:41 -0400

    initial commit

---
+++ weekdays/index.android.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class weekdays extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Days of the week:
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+
+
+AppRegistry.registerComponent('weekdays', () => weekdays);
