ba24ed70abacfc0e25a727cba802991adbc639c8
Kevin Wong <kevinwong_895@hotmail.com>
2017-08-05 20:51:26 -0400

    adding folder

---
+++ LearnPython/IntroductionToML/irisML.py
@@ -0,0 +1,22 @@
+from sklearn.datasets import load_iris
+import pandas as pd
+import mglearn
+import matplotlib.pyplot as plt
+
+iris_dataset = load_iris()
+print("Keys of iris_dataset: {}".format(iris_dataset.keys()))
+print(iris_dataset['DESCR'][:193] + "\n...")
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+# create dataframe from data in X_train
+# label the columns using the strings in iris_dataset.feature_names
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+# create a scatter matrix from the dataframe, color by y_train
+pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+                           hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+plt.show()
+
