79ebd59f8dc687c4f2afc12717e37a8c217c19a4
Kevin Wong <kevinwong_895@hotmail.com>
2017-11-15 19:34:36 -0800

    Finish RandomWiki

--- LearnPython/RandomWiki/main.py
+++ LearnPython/RandomWiki/main.py
@@ -1,6 +1,24 @@
+import webbrowser
+import requests
+import json
 
 def main():
-    print("hello");
+    print("-----Hello, Welcome to Random Wikipedia Article getter-----");
+    while True:
+        response = requests.get("https://en.wikipedia.org/w/api.php?action=query&list=random&rnnamespace=0&rnlimit=10&format=json")
+        jsonResponse = response.json()['query']['random']
+        for article in jsonResponse:
+            accept = input("Would you like to read " + article['title'] + " ? (Y/N)\n")
+            if accept == "Y":
+                webbrowser.open("https://en.wikipedia.org/wiki?curid=" + str(article["id"]))
+            else:
+                continue
+
+        continueVar = input("Would you like to continue? (Y/N\n")
+        if continueVar == "N":
+            break
+        else:
+            continue
 
 if __name__ == '__main__':
     main()
