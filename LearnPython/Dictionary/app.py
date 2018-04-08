1e0bedf2b2628f5d6d20258d5b8c478bf868b021
Kevin Wong <kevinwong_895@hotmail.com>
2018-04-08 10:39:52 -0700

    Adding difflib

--- LearnPython/Dictionary/app.py
+++ LearnPython/Dictionary/app.py
@@ -1,11 +1,17 @@
 import requests
 import json
+from difflib import get_close_matches
 
 # Enter Oxford Dictionary API Keys
-app_id = ''
-app_key = ''
+app_id = '8656f381'
+app_key = '79e9abc768d4f6bc125514b11388c63d'
 
 language = 'en'
+
+data = json.load(open("words_dictionary.json"))
+
+def summarizeOutput(json):
+    print(json.results)
 
 # Make Request to get JSON data
 def make_request(word):
@@ -15,9 +21,16 @@ def make_request(word):
     r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
 
     if r.status_code != 200:
-        print("No word found. Please try again")
+        if len(get_close_matches(word, data.keys())) > 0:
+            yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+            if yn == "Y":
+                make_request(get_close_matches(word, data.keys())[0])
+            elif yn == "N":
+                print("No word found. Please try again")
+            else:
+                print("Did not understand entry")
     else:
-        print("json \n" + json.dumps(r.json()))
+        print(r.json())
 
 def isEmpty(word):
     if word.strip() == "":
